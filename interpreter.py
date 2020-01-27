cead6078f63c31cb8fe329d4a9a01660183adb7f
atas <andy.torkin@gmail.com>
2020-01-27 17:31:12 +0200

    adding utests files to .gitignore

--- interpreter.py
+++ interpreter.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python
 # https://www.codewars.com/kata/58e61f3d8ff24f774400002c/train/python
 
+
 def assembler_interpreter(program):
+    pass
+
+if __name__ == "__main__":
     pass
