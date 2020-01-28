9c89c45708974f98c3a858eb34cf0b2b0f933100
atas <andy.torkin@gmail.com>
2020-01-28 15:06:13 +0200

    Adding main routine

--- interpreter.py
+++ interpreter.py
@@ -1,9 +1,21 @@
 #!/usr/bin/env python
 # https://www.codewars.com/kata/58e61f3d8ff24f774400002c/train/python
 
+import sys
 
 def assembler_interpreter(program):
-    pass
+    return 0
 
 if __name__ == "__main__":
-    pass
+    if len(sys.argv) != 2:
+        print("Usage: interpreter.py [program path]")
+        exit(-1)
+    try:
+        f = open(sys.argv[1], 'r')
+    except:
+        print("Wrong arg!")
+        exit(-1)
+
+    prog = f.read().splitlines()
+    output = assembler_interpreter(prog)
+    print(output)
