4932ae7c003bb6ed299b1d1f87cd1160ccc1bed5
Andrey Torkin <andy.torkin@gmail.com>
2021-06-17 16:46:22 +0300

    Implemented elemental words kata

---
+++ elemental_words.py
@@ -0,0 +1,44 @@
+ELEMENTS = {'H': 'Hydrogen', 'He': 'Helium', 'Li': 'Lithium', 'Be': 'Beryllium', 'B': 'Boron', 'C': 'Carbon', 'N': 'Nitrogen', 'O': 'Oxygen', 'F': 'Fluorine', 'Ne': 'Neon', 'Na': 'Sodium', 'Mg': 'Magnesium', 'Al': 'Aluminium', 'Si': 'Silicon', 'P': 'Phosphorus', 'S': 'Sulfur', 'Cl': 'Chlorine', 'Ar': 'Argon', 'K': 'Potassium', 'Ca': 'Calcium', 'Sc': 'Scandium', 'Ti': 'Titanium', 'V': 'Vanadium', 'Cr': 'Chromium', 'Mn': 'Manganese', 'Fe': 'Iron', 'Co': 'Cobalt', 'Ni': 'Nickel', 'Cu': 'Copper', 'Zn': 'Zinc', 'Ga': 'Gallium', 'Ge': 'Germanium', 'As': 'Arsenic', 'Se': 'Selenium', 'Br': 'Bromine', 'Kr': 'Krypton', 'Rb': 'Rubidium', 'Sr': 'Strontium', 'Y': 'Yttrium', 'Zr': 'Zirconium', 'Nb': 'Niobium', 'Mo': 'Molybdenum', 'Tc': 'Technetium', 'Ru': 'Ruthenium', 'Rh': 'Rhodium', 'Pd': 'Palladium', 'Ag': 'Silver', 'Cd': 'Cadmium', 'In': 'Indium', 'Sn': 'Tin', 'Sb': 'Antimony', 'Te': 'Tellurium', 'I': 'Iodine', 'Xe': 'Xenon', 'Cs': 'Caesium', 'Ba': 'Barium', 'La': 'Lanthanum', 'Ce': 'Cerium', 'Pr': 'Praseodymium', 'Nd': 'Neodymium', 'Pm': 'Promethium', 'Sm': 'Samarium', 'Eu': 'Europium', 'Gd': 'Gadolinium', 'Tb': 'Terbium', 'Dy': 'Dysprosium', 'Ho': 'Holmium', 'Er': 'Erbium', 'Tm': 'Thulium', 'Yb': 'Ytterbium', 'Lu': 'Lutetium', 'Hf': 'Hafnium', 'Ta': 'Tantalum', 'W': 'Tungsten', 'Re': 'Rhenium', 'Os': 'Osmium', 'Ir': 'Iridium', 'Pt': 'Platinum', 'Au': 'Gold', 'Hg': 'Mercury', 'Tl': 'Thallium', 'Pb': 'Lead', 'Bi': 'Bismuth', 'Po': 'Polonium', 'At': 'Astatine', 'Rn': 'Radon', 'Fr': 'Francium', 'Ra': 'Radium', 'Ac': 'Actinium', 'Th': 'Thorium', 'Pa': 'Protactinium', 'U': 'Uranium', 'Np': 'Neptunium', 'Pu': 'Plutonium', 'Am': 'Americium', 'Cm': 'Curium', 'Bk': 'Berkelium', 'Cf': 'Californium', 'Es': 'Einsteinium', 'Fm': 'Fermium', 'Md': 'Mendelevium', 'No': 'Nobelium', 'Lr': 'Lawrencium', 'Rf': 'Rutherfordium', 'Db': 'Dubnium', 'Sg': 'Seaborgium', 'Bh': 'Bohrium', 'Hs': 'Hassium', 'Mt': 'Meitnerium', 'Ds': 'Darmstadtium', 'Rg': 'Roentgenium', 'Cn': 'Copernicium', 'Uut': 'Ununtrium', 'Fl': 'Flerovium', 'Uup': 'Ununpentium', 'Lv': 'Livermorium', 'Uus': 'Ununseptium', 'Uuo': 'Ununoctium'} # noqa
+elements = tuple(el.lower() for el in ELEMENTS.keys())
+
+
+def recursive_search(word):
+    if not word:
+        yield None
+
+    chains = []
+    for el in elements:
+        if el == word[:len(el)]:
+            for chain in recursive_search(word[len(el):]):
+                if chain:
+                    if chain[-1][-1] == word[-1]:
+                        chains.append([el, *chain])
+                else:
+                    yield [el]
+
+    for chain in chains:
+        yield chain
+
+
+def elemental_forms(word):
+    return [[f"{ELEMENTS[element.capitalize()]} ({element.capitalize()})"
+             for element in chain]
+            for chain in recursive_search(word.lower())]
+
+
+if __name__ == "__main__":
+    testcases = {
+        'Yes': [['Yttrium (Y)', 'Einsteinium (Es)']],
+        'beach': [['Beryllium (Be)', 'Actinium (Ac)', 'Hydrogen (H)']],
+        'snack': [['Sulfur (S)', 'Nitrogen (N)', 'Actinium (Ac)', 'Potassium (K)'], # noqa
+                  ['Sulfur (S)', 'Sodium (Na)', 'Carbon (C)', 'Potassium (K)'],
+                  ['Tin (Sn)', 'Actinium (Ac)', 'Potassium (K)']]
+    }
+    for word, expected_result in testcases.items():
+        result = elemental_forms(word)
+        if result and result == expected_result:
+            print(f'Word: {word} -> Passed')
+        else:
+            print(f"""Word: {word} -> Wrong
+                    Expected: {expected_result}
+                    Returned: {result}""")
